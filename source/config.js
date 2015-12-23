4de9ced77f6d5590aacec7c1b27ba0216ebafb45
Matt Poegel <mpoegel@comcast.net>
2015-12-22 22:58:47 -0500

    added connect-flash to package.json; catch missing config file

--- source/config.js
+++ source/config.js
@@ -1,11 +1,17 @@
-var settings = require('./config.json'),
-	config = {};
+var settings = {},
+    config = {};
+try {
+  settings = require('./config.json');
+} catch(e) {
+  settings = {};
+}
 
 config.mongo_url = settings.mongo_url || '';
 config.port = settings.port || 8000;
 config.root_url = settings.root_url || 'http://localhost:' + config.port;
 
 config.email = {};
+settings.email = settings.email || {};
 config.email.address = settings.email.address || '';
 config.email.password = settings.email.password || '';
 config.email.from = settings.email.from || '';
